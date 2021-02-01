5e9da74e2238b3f308726d6321ff011cf68ebf10
hm22-22 <celine1994@qq.com>
2021-02-01 19:38:05 +0100

    update image API

--- src/main/java/com/apt612/depaybackend/controller/api/ImageAPI.java
+++ src/main/java/com/apt612/depaybackend/controller/api/ImageAPI.java
@@ -3,6 +3,7 @@ package com.apt612.depaybackend.controller.api;
 import com.apt612.depaybackend.controller.security.annotations.Authenticated;
 import com.apt612.depaybackend.model.Image;
 import com.apt612.depaybackend.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/item/{itemId}/image")
+@RequestMapping("/items/{itemId}/images")
 @CrossOrigin
 public class ImageAPI {
+    @Autowired
     ImageService imageService;
 
 
@@ -28,8 +30,11 @@ public class ImageAPI {
         return new ResponseEntity<List<Image>>(imageService.getImagesByItemId(itemId),HttpStatus.FOUND);
     }
 
-    @GetMapping("first")
-    public  ResponseEntity<Image> getFirstImageOfItem(@PathVariable("itemId") String itemId) {
-        return new ResponseEntity<Image>(imageService.getOneImageByItemId(itemId),HttpStatus.FOUND);
+    @GetMapping(value = "/first")
+    public  ResponseEntity<String> getFirstImageOfItem(@PathVariable("itemId") String itemId) {
+       Image image = imageService.getOneImageByItemId(itemId);
+       if(image != null) return new ResponseEntity<String>(image.getImageBase64(), HttpStatus.FOUND);
+
+       return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 }
